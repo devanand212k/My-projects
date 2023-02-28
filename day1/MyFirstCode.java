fb943d52051943bdd7cd1d5f7ca374fc508d2beb
devanand212k <126375240+devanand212k@users.noreply.github.com>
2023-02-28 21:24:42 +0530

    Sample-MyFirstCode only

---
+++ day1/MyFirstCode.java
@@ -0,0 +1,9 @@
+package week1.day1;
+
+import java.security.DomainCombiner;
+
+public class MyFirstCode {
+	public static void main(String[] args) {
+		System.out.println("This is my First Code");
+	}
+}
